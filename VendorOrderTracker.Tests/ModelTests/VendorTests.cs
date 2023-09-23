46195a19ee5f4356407b1906b5a4734bcea350be
jessicahattig <jessicahattig@gmail.com>
2023-09-23 13:22:56 -0700

    Vendor.cs, second test, red and green phase.

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -15,5 +15,19 @@ namespace VendorOrderTracker
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Category";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
   }
 }
