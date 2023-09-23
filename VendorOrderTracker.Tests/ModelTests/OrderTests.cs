985a1180268ede7405d36f068a44d1d4b7ddb1a2
jessicahattig <jessicahattig@gmail.com>
2023-09-23 14:04:20 -0700

    Order.cs, second test, red and green phase.

--- VendorOrderTracker.Tests/ModelTests/OrderTests.cs
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -15,5 +15,19 @@ namespace VendorOrderTracker
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "3 loafs of bread.";
+
+      //Act
+      Order newOrder = new Order(description);
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
