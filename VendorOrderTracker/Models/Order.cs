77232381a941a9a1710c71a70ce5814f7a67d690
jessicahattig <jessicahattig@gmail.com>
2023-09-23 14:50:16 -0700

    WIP: Views

--- VendorOrderTracker/Models/Order.cs
+++ VendorOrderTracker/Models/Order.cs
@@ -5,12 +5,14 @@ namespace VendorOrderTracker.Models
   public class Order
   {
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Order> _instances = new List<Order> { };
 
   public Order(string description)
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
   public static List<Order> GetAll()
