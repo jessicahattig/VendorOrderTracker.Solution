c5042269d11db3f2c07c063fe56b505a2f4d88d7
jessicahattig <jessicahattig@gmail.com>
2023-09-23 14:02:43 -0700

    Order.cs, first test, red and green phase.

---
+++ VendorOrderTracker.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker
+{
+  [TestClass]
+  public class OrderTests
+  {
+
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  }
+}
